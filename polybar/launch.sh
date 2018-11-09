7e415ad84433ac030feba1b394383e67232991da
Saad Rasheed <saarasheed@outlook.com>
2018-11-09 14:53:28 -0600

    updating work dual polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -9,8 +9,10 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-        MONITOR=$m polybar --reload main &
+        MONITOR=$m polybar --reload top &
+        MONITOR=$m polybar --reload bottom
     done
 else
-    polybar --reload main &
+    polybar --reload top &
+    polybar --reload bottom &
 fi
